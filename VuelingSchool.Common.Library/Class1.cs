b54af57667d61335d787dfb4c6b4b174aa58e3b1
Jordy Enrrique García Intriago <jordygarcia1001@hotmail.com>
2019-02-13 12:29:48 +0100

    Initinial Commit. Structure Created. Close#1

---
+++ VuelingSchool.Common.Library/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingSchool.Common.Library
+{
+    public class Class1
+    {
+        
+    }
+}
---
+++ VuelingSchool.DataAccess.Repository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingSchool.DataAccess.Repository
+{
+    public class Class1
+    {
+    }
+}
